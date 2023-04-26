e87ef07b054d2ea5bcc1aa6e51d738e0bce5858d
Haydar Alçin <hydralcin@gmail.com>
2023-04-26 17:11:30 +0300

    v0.2

--- BouncingBall/Assets/Scripts/TriangleRotate.cs
+++ BouncingBall/Assets/Scripts/TriangleRotate.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class TriangleRotate : MonoBehaviour
 {
+    public Transform nextCheckpoint;
+    public Vector2 road;
     
+
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+
+        if (this.gameObject.tag == "checkpoint3")
+        {
+            road = (nextCheckpoint.position - transform.position).normalized;
+        }
+
+        else
+        {
+            road = (nextCheckpoint.position - transform.position).normalized;
+        }
+
+
+
+    }
 }
