698b61009109094d10caa8664fd0ecb30b2a5508
Haydar Alçin <hydralcin@gmail.com>
2023-05-02 19:54:50 +0300

    v0.5

--- BouncingBall/Assets/Scripts/BallController.cs
+++ BouncingBall/Assets/Scripts/BallController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BallController : MonoBehaviour
 {
     Rigidbody2D rb;
+
+    public GameObject gameOverPnl;
 
     //Her dokunustaki kuvvet
     public float force;
@@ -14,31 +18,48 @@ public class BallController : MonoBehaviour
 
     
     
-    private void Start()
+    private void Awake()
     {
         gameStart = true;
+        Time.timeScale = 1f;
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        FirstTouch();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+        
+             if (gameStart == true)
+             {
+                gameStart = false;
+                rb.bodyType = RigidbodyType2D.Dynamic;
+                rb.AddRelativeForce(new Vector2(0, -25f * Time.deltaTime));
+            }
+             rb.velocity = new Vector2(0, 1);
+             rb.AddRelativeForce(new Vector2(0, force * Time.deltaTime));
+          
+        }
     }
 
 
-    public void FirstTouch()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (Input.GetAxisRaw("Jump")==1)
-        {   
+        if (other.tag=="enemy")
+        {
+            Debug.Log("GameOver");
+            gameOverPnl.SetActive(true);
+            Time.timeScale = 0f;
 
-            if (gameStart==true)
-            {
-                gameStart = false;
-                rb.bodyType = RigidbodyType2D.Dynamic;
-            }
-            rb.velocity =  new Vector2(0,1);
-            rb.AddRelativeForce(new Vector2(0, force * Time.deltaTime));
         }
+    }
+
+
+
+    public void TryAgainBtn() 
+    {
         
+        SceneManager.LoadScene(0);
+
     }
 }
